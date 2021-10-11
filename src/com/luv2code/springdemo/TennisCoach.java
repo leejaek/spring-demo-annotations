e4471cbe94ee9863d7b681070d6aee95e615a5a8
leejaek <leejh9049@gmail.com>
2021-10-11 18:28:19 +0900

    Spring Configuration with Java Annotations - Dependency Injection : Practice Activity #6

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -5,17 +5,31 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
-    @Qualifier("randomFortuneService")
+    @Qualifier("readFileRandomFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
     public TennisCoach() {
         System.out.println(">> TennisCoach inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
     }
 
     // define a setter method
